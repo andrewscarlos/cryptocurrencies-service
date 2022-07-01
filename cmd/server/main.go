495b2dfbe6798452a4c2a1b9dc5cf5aa4ed63825
andrewscarlos <andrews.knct@gmail.com>
2022-06-30 22:40:18 -0300

    feat: add grpc server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"cryptocurrencies-service/pb"
+	"cryptocurrencies-service/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
 
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterAssetServiceServer(grpcServer, service.NewAssetService())
+	reflection.Register(grpcServer)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Could not serve: %v", err)
+	}
 }
