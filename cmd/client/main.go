a85d81268622b92f93600f8a61c85f280e7d8261
andrewscarlos <andrews.knct@gmail.com>
2022-07-04 22:35:27 -0300

    feat: fixing client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"cryptocurrencies-service/pb"
-	"fmt"
 	"google.golang.org/grpc"
 	"log"
 )
@@ -14,21 +12,21 @@ func main() {
 		log.Fatalf("Could not connect to gRPC Server %v", err)
 	}
 	defer connection.Close()
-	client := pb.NewAssetServiceClient(connection)
-	Insert(client)
+	pb.NewAssetServiceClient(connection)
+	//Insert(client)
 }
 
-func Insert(client pb.AssetServiceClient) {
-	req := &pb.Asset{
-		Id:         "0123456789ab0123456789ab", //bson.NewObjectId().String()
-		Address:    "HNLQ4sDKQFcDJegPwnwd3N39TBdRQW9Sfd",
-		Value:      50000,
-		Name:       "Jhon doe Coin",
-		Blockchain: "foo",
-	}
-	res, err := client.Insert(context.Background(), req)
-	if err != nil {
-		log.Fatalf("Could not make gRPC request: %v", err)
-	}
-	fmt.Println(res)
-}
+//func Insert(client pb.AssetServiceClient) {
+//	req := &pb.Asset{
+//		Id:         "0123456789ab0123456789ab", //bson.NewObjectId().String()
+//		Address:    "HNLQ4sDKQFcDJegPwnwd3N39TBdRQW9Sfd",
+//		Value:      50000,
+//		Name:       "Jhon doe Coin",
+//		Blockchain: "foo",
+//	}
+//	res, err := client.Insert(context.Background(), req)
+//	if err != nil {
+//		log.Fatalf("Could not make gRPC request: %v", err)
+//	}
+//	fmt.Println(res)
+//}
